c626efa9f5f639572197222cba09ec61c57579e8
murilo.lima <murilossl07@gmail.com>
2024-10-22 11:42:27 -0300

    adicionando controller completo

--- src/controller/ClienteController.ts
+++ src/controller/ClienteController.ts
@@ -1,30 +1,85 @@
 import { Request, Response } from "express";
 import { Cliente } from "../model/Cliente";
 
+interface ClienteDTO {
+    nome: string,
+    cpf: string,
+    telefone: string
+}
+
 /**
-* A classe `ClienteController` estende a classe `Cliente` e é responsável por controlar as requisições relacionadas aos clientes.
-* 
-* - Como um controlador em uma API REST, esta classe gerencia as operações relacionadas ao recurso "cliente".
-* - Herdando de `Cliente`, ela pode acessar os métodos e propriedades da classe base.
-*/
+ * A classe `CarroController` estende a classe `Cliente` e é responsável por controlar as requisições relacionadas aos cliente.
+ * 
+ * - Esta classe atua como um controlador dentro de uma API REST, gerenciando as operações relacionadas ao recurso "cliente".
+ * - Herdando de `Cliente`, ela pode acessar métodos e propriedades da classe base.
+ */
 export class ClienteController extends Cliente {
 
     /**
-     * Lista todos os clientes.
-     * @param req Objeto de requisição HTTP.
-     * @param res Objeto de resposta HTTP.
-     * @returns Lista de clientes em formato JSON com status 200 em caso de sucesso.
-     * @throws Retorna um status 400 com uma mensagem de erro caso ocorra uma falha ao acessar a listagem de clientes.
-     */
+    * Lista todos os cliente.
+    * @param req Objeto de requisição HTTP.
+    * @param res Objeto de resposta HTTP.
+    * @returns Lista de cliente em formato JSON com status 200 em caso de sucesso.
+    * @throws Retorna um status 400 com uma mensagem de erro caso ocorra uma falha ao acessar a listagem de cliente.
+    */
     static async todos(req: Request, res: Response): Promise<Response> {
         try {
-            const listaDeClientes = await Cliente.listagemClientes();
-            console.log(listaDeClientes);
-            
-            return res.status(200).json(listaDeClientes);
+            // acessa a função de listar os cliente e armazena o resultado
+            const listaDeCliente = await Cliente.listagemClientes();
+
+            // retorna a lista de cliente há quem fez a requisição web
+            return res.status(200).json(listaDeCliente);
         } catch (error) {
-            console.log('Erro ao acessar listagem de carros');
-            return res.status(400).json({ mensagem: "Não foi possível acessar a listagem de carros" });
+            // lança uma mensagem de erro no console
+            console.log('Erro ao acessar listagem de cliente');
+
+            // retorna uma mensagem de erro há quem chamou a mensagem
+            return res.status(400).json({ mensagem: "Não foi possível acessar a listagem de cliente" });
+        }
+    }
+
+    /**
+    * Método controller para cadastrar um novo cliente.
+    * 
+    * Esta função recebe uma requisição HTTP contendo os dados de um cliente no corpo da requisição
+    * e tenta cadastrar este cliente no banco de dados utilizando a função `cadastroCliente`. Caso o cadastro 
+    * seja bem-sucedido, retorna uma resposta HTTP 200 com uma mensagem de sucesso. Caso contrário, retorna
+    * uma resposta HTTP 400 com uma mensagem de erro.
+    * 
+    * @param {Request} req - Objeto de requisição HTTP, contendo o corpo com os dados do cliente no formato `CarroDTO`.
+    * @param {Response} res - Objeto de resposta HTTP usado para retornar o status e a mensagem ao cliente.
+    * @returns {Promise<Response>} - Retorna uma resposta HTTP com o status 200 em caso de sucesso, ou 400 em caso de erro.
+    * 
+    * @throws {Error} - Se ocorrer um erro durante o processo de cadastro, uma mensagem é exibida no console e uma 
+    *                   resposta HTTP 400 com uma mensagem de erro é enviada ao cliente.
+    */
+    static async novo(req: Request, res: Response): Promise<Response> {
+        try {
+            // recuperando informações do corpo da requisição e colocando em um objeto da interface CarroDTO
+            const clienteRecebido: ClienteDTO = req.body;
+
+            // instanciando um objeto do tipo cliente com as informações recebidas
+            const novoCliente = new Cliente(clienteRecebido.nome, 
+                                        clienteRecebido.cpf, 
+                                        clienteRecebido.telefone,);
+
+            // Chama a função de cadastro passando o objeto como parâmetro
+            const repostaClasse = await Cliente.cadastroCliente(novoCliente);
+
+            // verifica a resposta da função
+            if(repostaClasse) {
+                // retornar uma mensagem de sucesso
+                return res.status(200).json({ mensagem: "Cliente cadastrado com sucesso!" });
+            } else {
+                // retorno uma mensagem de erro
+                return res.status(400).json({ mensagem: "Erro ao cadastra o cliente. Entre em contato com o administrador do sistema."})
+            } 
+        } catch (error) {
+            // lança uma mensagem de erro no console
+            console.log(`Erro ao cadastrar um cliente. ${error}`);
+
+            // retorna uma mensagem de erro há quem chamou a mensagem
+            return res.status(400).json({ mensagem: "Não foi possível cadastrar o cliente. Entre em contato com o administrador do sistema." });
         }
     }
 }
--- src/controller/PedidoVendaController.ts
+++ src/controller/PedidoVendaController.ts
@@ -1,29 +1,88 @@
 import { Request, Response } from "express";
 import { PedidoVenda } from "../model/PedidoVenda";
 
+interface PedidoVendaDTO {
+    dataPedido: Date,
+    valorPedido: number,
+    id_cliente: number,
+    id_carro: number
+}
+
 /**
- * A classe `PedidoVendaController` estende a classe `PedidoVenda` e é responsável por controlar as requisições relacionadas aos pedidos de venda.
+ * A classe `CarroController` estende a classe `Pedidos` e é responsável por controlar as requisições relacionadas aos pedidos.
  * 
- * - Como um controlador dentro de uma API REST, esta classe gerencia as operações relacionadas ao recurso "pedido de venda".
- * - Herdando de `PedidoVenda`, ela pode acessar os métodos e propriedades da classe base.
+ * - Esta classe atua como um controlador dentro de uma API REST, gerenciando as operações relacionadas ao recurso "pedidos".
+ * - Herdando de `Pedidos`, ela pode acessar métodos e propriedades da classe base.
  */
 export class PedidoVendaController extends PedidoVenda {
 
     /**
-     * Lista todos os pedidos de venda.
-     * @param req Objeto de requisição HTTP.
-     * @param res Objeto de resposta HTTP.
-     * @returns Lista de pedidos de venda em formato JSON com status 200 em caso de sucesso.
-     * @throws Retorna um status 400 com uma mensagem de erro caso ocorra uma falha ao acessar a listagem de pedidos de venda.
-     */
+    * Lista todos os pedidos.
+    * @param req Objeto de requisição HTTP.
+    * @param res Objeto de resposta HTTP.
+    * @returns Lista de pedidos em formato JSON com status 200 em caso de sucesso.
+    * @throws Retorna um status 400 com uma mensagem de erro caso ocorra uma falha ao acessar a listagem de pedidos.
+    */
     static async todos(req: Request, res: Response): Promise<Response> {
         try {
-            const listaPedidos = await PedidoVenda.listagemPedidos();
+            // acessa a função de listar os pedidos e armazena o resultado
+            const listaDePedidos = await PedidoVenda.listagemPedidos();
 
-            return res.status(200).json(listaPedidos);
+            // retorna a lista de pedidos há quem fez a requisição web
+            return res.status(200).json(listaDePedidos);
         } catch (error) {
-            console.log('Erro ao acessar listagem de carros');
-            return res.status(400).json({ mensagem: "Não foi possível acessar a listagem de carros" });
+            // lança uma mensagem de erro no console
+            console.log('Erro ao acessar listagem de pedidos');
+
+            // retorna uma mensagem de erro há quem chamou a mensagem
+            return res.status(400).json({ mensagem: "Não foi possível acessar a listagem de pedidos" });
+        }
+    }
+
+    /**
+    * Método controller para cadastrar um novo pedidos.
+    * 
+    * Esta função recebe uma requisição HTTP contendo os dados de um pedidos no corpo da requisição
+    * e tenta cadastrar este pedidos no banco de dados utilizando a função `cadastroCliente`. Caso o cadastro 
+    * seja bem-sucedido, retorna uma resposta HTTP 200 com uma mensagem de sucesso. Caso contrário, retorna
+    * uma resposta HTTP 400 com uma mensagem de erro.
+    * 
+    * @param {Request} req - Objeto de requisição HTTP, contendo o corpo com os dados do pedidos no formato `CarroDTO`.
+    * @param {Response} res - Objeto de resposta HTTP usado para retornar o status e a mensagem ao pedidos.
+    * @returns {Promise<Response>} - Retorna uma resposta HTTP com o status 200 em caso de sucesso, ou 400 em caso de erro.
+    * 
+    * @throws {Error} - Se ocorrer um erro durante o processo de cadastro, uma mensagem é exibida no console e uma 
+    *                   resposta HTTP 400 com uma mensagem de erro é enviada ao pedidos.
+    */
+    static async novo(req: Request, res: Response): Promise<Response> {
+        try {
+            // recuperando informações do corpo da requisição e colocando em um objeto da interface CarroDTO
+            const pedidoRecebido: PedidoVendaDTO = req.body;
+
+            // instanciando um objeto do tipo pedidos com as informações recebidas
+            const novoPedido = new PedidoVenda(
+                                        pedidoRecebido.id_carro,
+                                        pedidoRecebido.id_cliente,
+                                        pedidoRecebido.dataPedido, 
+                                        pedidoRecebido.valorPedido);
+
+            // Chama a função de cadastro passando o objeto como parâmetro
+            const repostaClasse = await PedidoVenda.cadastroPedido(novoPedido);
+
+            // verifica a resposta da função
+            if(repostaClasse) {
+                // retornar uma mensagem de sucesso
+                return res.status(200).json({ mensagem: "Pedidos cadastrado com sucesso!" });
+            } else {
+                // retorno uma mensagem de erro
+                return res.status(400).json({ mensagem: "Erro ao cadastra o pedidos. Entre em contato com o administrador do sistema."})
+            } 
+        } catch (error) {
+            // lança uma mensagem de erro no console
+            console.log(`Erro ao cadastrar um pedidos. ${error}`);
+
+            // retorna uma mensagem de erro há quem chamou a mensagem
+            return res.status(400).json({ mensagem: "Não foi possível cadastrar o pedidos. Entre em contato com o administrador do sistema." });
         }
     }
 }
